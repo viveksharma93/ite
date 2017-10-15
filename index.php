47dcc9539ce0d8c2aebf5a70de108277937850f0
Vivek <viveksharma1202@gmail.com>
2017-10-15 18:52:25 +0530

    updated

--- index.php
+++ index.php
@@ -239,8 +239,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item1.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme</a> </h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training</a> </h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item1.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
@@ -252,8 +252,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item2.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme</a></h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training</a></h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item2.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
@@ -265,8 +265,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item3.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme </a></h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training </a></h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item3.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
@@ -278,8 +278,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item4.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme </a></h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training </a></h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item4.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
@@ -291,8 +291,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item5.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme</a></h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training</a></h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item5.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
@@ -304,8 +304,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item6.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme </a></h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training </a></h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item6.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
@@ -317,8 +317,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item7.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme </a></h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training </a></h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item7.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
@@ -330,8 +330,8 @@
                         <img class="img-responsive" src="images/portfolio/recent/item8.png" alt="">
                         <div class="overlay">
                             <div class="recent-work-inner">
-                                <h3><a href="#">Business theme </a></h3>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                <h3><a href="#">Training </a></h3>
+                                <p>Successfully delivered docker training to Wipro employees</p>
                                 <a class="preview" href="images/portfolio/full/item8.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                             </div>
                         </div>
